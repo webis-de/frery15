e16c29b46ad919c06351df73eb98df8dc194d41b
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2017-12-05 14:05:23 +0100

    Add average and standard deviation of words per sentence

--- representation.py
+++ representation.py
@@ -1,22 +1,31 @@
 from collections import Counter
+import re
+import numpy as np
 
-#def representation(document):
-#    raise NotImplementedError('Please use an implemented representation')
 
 def character_n_grams(n, document, corpus):
     return -1
 
+
 def word_n_grams(n, document, corpus, with_30_percent_most_frequent=True, with_stop_words=True):
     return -1
 
+
 def avg_stdev_words_per_sentence(document):
-    return -1
+    document_splitted = re.split('. |! |\? ', document)
+    # TODO: Maybe remove newlines
+    words_per_sentence = []
+    for sentence in document_splitted:
+        words_per_sentence.append(len(sentence.split()))
+    return [np.average(words_per_sentence), np.std(words_per_sentence)]
+
 
 def vocubalary_diversity(document):
     document = "".join(c for c in document if c not in ':;?!.,()')
     document_splitted = document.split()
     counter = Counter(document_splitted)
     return len(list(counter)) / len(document_splitted)
+
 
 def avg_marks(document):
     document_splitted = document.split('.')
@@ -27,6 +36,7 @@ def avg_marks(document):
     for element in list(counter):
         representation.append(counter[element]/number_of_sentences)
     return representation
+
 
 def concatenation(document):
     features = avg_stdev_words_per_sentence(document)
