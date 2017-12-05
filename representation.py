e2a9157477fcc81840203c320a9f4809cb307d61
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2017-12-05 13:40:54 +0100

    Add vocabulary diversity and average marks per sentence

--- representation.py
+++ representation.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 #def representation(document):
 #    raise NotImplementedError('Please use an implemented representation')
 
@@ -11,10 +13,20 @@ def avg_stdev_words_per_sentence(document):
     return -1
 
 def vocubalary_diversity(document):
-    return -1
+    document = "".join(c for c in document if c not in ':;?!.,()')
+    document_splitted = document.split()
+    counter = Counter(document_splitted)
+    return len(list(counter)) / len(document_splitted)
 
 def avg_marks(document):
-    return -1
+    document_splitted = document.split('.')
+    number_of_sentences = len(document_splitted)
+    marks = "".join(c for c in document if c in ',;:()!?')
+    counter = Counter(marks)
+    representation = []
+    for element in list(counter):
+        representation.append(counter[element]/number_of_sentences)
+    return representation
 
 def concatenation(document):
     features = avg_stdev_words_per_sentence(document)
