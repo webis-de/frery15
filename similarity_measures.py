44112ffad7d23008b071a158de91c6327f331ac9
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2017-12-06 22:08:50 +0100

    Output just the number without list in list

--- similarity_measures.py
+++ similarity_measures.py
@@ -2,11 +2,13 @@ from sklearn.metrics import pairwise
 from scipy.stats.stats import pearsonr
 import numpy as np
 
+
 def similarity(vector1, vector2):
     raise NotImplementedError('Please use an implemented distance method')
 
+
 def cosine_similarity(vector1, vector2):
-    return pairwise.cosine_similarity(vector1, vector2)
+    return pairwise.cosine_similarity(vector1, vector2)[0][0]
 
 
 def correlation_coefficient(vector1, vector2):
