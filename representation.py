084be14c289d8e3222ec4fac9cd34236ef17f6a9
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2017-12-06 22:09:16 +0100

    Implement word and character n-grams

--- representation.py
+++ representation.py
@@ -1,14 +1,20 @@
 from collections import Counter
 import re
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def character_n_grams(n, document, corpus):
-    return -1
+    vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(n,n))
+    vectorizer = vectorizer.fit(corpus)
+    matrix = vectorizer.transform([document])
+    assert matrix.max() != 0
+    return matrix
 
 
-def word_n_grams(n, document, corpus, with_30_percent_most_frequent=True, with_stop_words=True):
-    return -1
+def word_n_grams(n, document, corpus, max_df=1.0, stop_words=None):
+    vectorizer = TfidfVectorizer(analyzer='word', ngram_range=(n, n), stop_words=stop_words, max_df=max_df).fit(corpus)
+    return vectorizer.transform([document])
 
 
 def avg_stdev_words_per_sentence(document):
@@ -20,7 +26,7 @@ def avg_stdev_words_per_sentence(document):
     return [np.average(words_per_sentence), np.std(words_per_sentence)]
 
 
-def vocubalary_diversity(document):
+def vocabulary_diversity(document):
     document = "".join(c for c in document if c not in ':;?!.,()')
     document_splitted = document.split()
     counter = Counter(document_splitted)
@@ -40,6 +46,6 @@ def avg_marks(document):
 
 def concatenation(document):
     features = avg_stdev_words_per_sentence(document)
-    features.extend(vocubalary_diversity(document))
+    features.extend(vocabulary_diversity(document))
     features.extend(avg_marks(document))
     return features
