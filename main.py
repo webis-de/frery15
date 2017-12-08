c3e2ad80aa20e243ba85060b229a6743d6fbb845
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2017-12-09 00:11:01 +0100

    Add processing of representation spaces and features

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from preprocessing import may_download_training, may_unzip_corpus, load_text_corpus
+from preprocessing import may_download_training, may_unzip_corpus, load_text_corpora
+from dissimilarity_counter_method import dissimilarity_counter_method
+from representation_spaces import *
+from similarity_measures import cosine_similarity
+from features import count, mean
+import numpy as np
+from sklearn import tree
+from sklearn.model_selection import cross_val_score
 
 train_corpora_url = 'http://www.uni-weimar.de/medien/webis/corpora/corpus-pan-labs-09-today/pan-14/pan14-data/pan14-authorship-verification-training-corpus-2014-04-22.zip'
 train_corpora_dir = 'pan14-authorship-verification-training-corpus-2014-04-22'
@@ -8,7 +15,55 @@ def main():
     may_download_training(train_corpora_url, data_dir, train_corpora_dir)
     may_unzip_corpus(data_dir + '/' + train_corpora_dir, data_dir, train_corpora_dir)
 
-    load_text_corpus()
+    corpora = load_text_corpora(data_dir, train_corpora_dir)
+
+    for corpus in corpora:
+        corpus_appended = []
+        for problem in corpus:
+            [known_documents, unknown, _] = problem
+            corpus_appended.append(unknown)
+            for known_document in known_documents:
+                corpus_appended.append(known_document)
+
+        X = []
+        Y = []
+        for [known_documents, unknown, label] in corpus:
+            counts = []
+            means = []
+            features = []
+
+            for representation_space in [lambda document: representation_space1(document, corpus_appended),
+                                         lambda document: representation_space2(document, corpus_appended),
+                                         lambda document: representation_space3(document, corpus_appended),
+                                         lambda document: representation_space4(document, corpus_appended),
+                                         lambda document: representation_space5(document, corpus_appended),
+                                         #lambda document: representation_space6(document),
+                                         #lambda document: representation_space7(document),
+                                         #lambda document: representation_space8(document),
+                                         #lambda document: representation_space678(document)
+                                         ]:
+                known_documents_in_representation_space = []
+                for known_document in known_documents:
+                    known_documents_in_representation_space.append(representation_space(known_document))
+                unknown_document_in_representation_space = representation_space(unknown)
+                threshold = None
+                similarity_measure = cosine_similarity
+                #print(dissimilarity_counter_method(known_documents_in_representation_space,
+                #                                   unknown_document_in_representation_space, threshold=threshold,
+                #                                   similarity_measure=similarity_measure))
+                for known_document_in_representation_space in known_documents_in_representation_space:
+                    counts.append(count(known_document_in_representation_space, unknown_document_in_representation_space, similarity_measure))
+                    means.append(mean(known_document_in_representation_space, unknown_document_in_representation_space, similarity_measure))
+
+            features.extend(counts)
+            features.extend(means)
+            features.append(np.average(counts))
+            X.append(features)
+            Y.append(label)
+        clf = tree.DecisionTreeClassifier()
+        # TODO: Use auc score
+        scores = cross_val_score(clf, X, Y, cv=10, n_jobs=-1)
+        print(scores)
 
 
 if __name__ == '__main__':
