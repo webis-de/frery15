5f21aeb655b229d5a6f23e12ced96562fa7fd149
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2017-12-10 12:19:58 +0100

    Generalize the preprocessing methods

--- preprocessing.py
+++ preprocessing.py
@@ -12,7 +12,7 @@ else:
     from urllib import urlretrieve
 
 
-def may_download_training(url, prefix_dir, dir):
+def may_download_data(url, prefix_dir, dir):
     if not os.path.exists(prefix_dir):
         os.makedirs(prefix_dir)
     if not os.path.exists(prefix_dir + '/' + dir):
@@ -27,16 +27,16 @@ def may_download_training(url, prefix_dir, dir):
         os.remove(zip_file)
 
 
-def may_unzip_corpus(dir_zips, data_dir, train_corpora_dir):
+def may_unzip_corpus(dir_zips, data_dir, corpora_dir):
     for _, _, files in os.walk(dir_zips):
         for file in files:
-            if file.endswith(".zip") and not os.path.exists(data_dir + '/' + train_corpora_dir + '/' + file[:-4]):
-                with zipfile.ZipFile(data_dir + '/' + train_corpora_dir + '/' + file, "r") as z:
-                    z.extractall(data_dir + '/' + train_corpora_dir + '/')
+            if file.endswith(".zip") and not os.path.exists(data_dir + '/' + corpora_dir + '/' + file[:-4]):
+                with zipfile.ZipFile(data_dir + '/' + corpora_dir + '/' + file, "r") as z:
+                    z.extractall(data_dir + '/' + corpora_dir + '/')
 
                 # Unzipped file has an other name than zip file
                 # assert os.path.exists(data_dir+'/'+train_corpora_dir+'/'+file[:-4])
-                os.remove(data_dir + '/' + train_corpora_dir + '/' + file)
+                os.remove(data_dir + '/' + corpora_dir + '/' + file)
 
 
 def load_text_corpora(data_dir, train_corpora_dir):
