484eaf571cc848de3fd2366c21a36c83df33e886
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2017-12-24 00:40:30 +0100

    Submit all text files as jobs to process

--- transform_files.py
+++ transform_files.py
@@ -4,43 +4,70 @@ from multiprocessing import Pool
 from representation_spaces import *
 from main import attribution_dataset_data_dir
 import sys
-
+import pickle
+import time
 
 def transform_data():
     corpus_name = sys.argv[1]
+    assert corpus_name != ''
     dataset = attribution_dataset_data_dir + '/' + corpus_name
 
     pool = Pool(processes=30)
 
-    if not os.path.exists(os.path.join('corpora_texts', dataset)):
-        if not os.path.exists('corpora_texts'):
-            os.makedirs('corpora_texts')
-        candidates = jsonhandler.candidates
-        unknowns = jsonhandler.unknowns
-        jsonhandler.loadJson(dataset)
-        jsonhandler.loadTraining()
-        corpus = []
-        for author in candidates:
-            for file in jsonhandler.trainings[author]:
-                args = (author, file)
-                pool.map_async(transform_write_text,args,"test")
-        # TODO: Also for unknown texts
+    #if not os.path.exists(os.path.join('corpora_texts', dataset)):
+    #    if not os.path.exists('corpora_texts'):
+    #        os.makedirs('corpora_texts')
+    candidates = jsonhandler.candidates
+    unknowns = jsonhandler.unknowns
+    jsonhandler.loadJson(dataset)
+    jsonhandler.loadTraining()
+
+
+    corpus_file = open(os.path.join(dataset,'all_text_files.pickle'), "wb")
+    pickle.dump(corpus, corpus_file, protocol=pickle.HIGHEST_PROTOCOL)
+    corpus_file.close()
+
+    args = []
+    for author in candidates:
+        for file in jsonhandler.trainings[author]:
+            args.append((author, file, dataset))
+    result = pool.map_async(transform_write_text, args).get()
+    # TODO: Also for unknown texts
+    #result.wait()
+    pool.close()
+    pool.join()
+
 
 
 def transform_write_text(arg):
-    author, file = arg
+    (author, file, dataset) = arg
+    jsonhandler.loadJson(dataset)
     text = jsonhandler.getTrainingText(author, file)
-    for representation_space in [#lambda document: representation_space1(document, corpus_each_problem_as_one_text),
-                                     #lambda document: representation_space2(document, corpus_each_problem_as_one_text),
-                                     #lambda document: representation_space3(document, corpus_each_problem_as_one_text),
-                                     #lambda document: representation_space4(document, corpus_each_problem_as_one_text),
-                                     #lambda document: representation_space5(document, corpus_each_problem_as_one_text),
-                                     lambda document: representation_space6(document),
-                                     lambda document: representation_space7(document),
-                                     lambda document: representation_space8(document),
-                                     lambda document: representation_space678(document)]:
+    corpus_file = open(os.path.join(dataset, 'all_text_files.pickle'), "rb")
+    corpus = pickle.load(corpus_file)
+    corpus_file.close()
+    for representation_space in [representation_space6,
+                                 representation_space7,
+                                 representation_space8,
+                                 representation_space678]:
         if not jsonhandler.existTransformedTrainingText(author, file, representation_space.__name__):
+            print('File does not exist')
             content = representation_space(text)
             jsonhandler.pickleTransformedTrainingText(author, file, content, representation_space.__name__)
-        #else:
-        #    content = jsonhandler.loadTransformedTrainingText(author, file, representation_space.__name__)
+        else:
+            print('File already exists')
+            content = jsonhandler.loadTransformedTrainingText(author, file, representation_space.__name__)
+
+    for representation_space in [representation_space1, representation_space2, representation_space3,
+                                 representation_space4, representation_space5]:
+        if not jsonhandler.existTransformedTrainingText(author, file, representation_space.__name__):
+            print('File does not exist')
+            content = representation_space(text, corpus)
+            jsonhandler.pickleTransformedTrainingText(author, file, content, representation_space.__name__)
+        else:
+            print('File already exists')
+            content = jsonhandler.loadTransformedTrainingText(author, file, representation_space.__name__)
+    if not os.path.exists(os.path.join(dataset, author, file+'_'+representation_space1.__name__+'.pickle')):
+        raise FileNotFoundError("Ahh")
+
+transform_data()
